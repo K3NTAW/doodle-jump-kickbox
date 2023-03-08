3970106f121c686f7a4c54028f1ddfa2a0518a43
Kenta Laptop <kenta.waibel@gmail.com>
2023-03-08 11:10:01 +0100

    feat: adding a loop back to the home screen after death. feat: adding a game over screen.
    by Kenta Waibel

--- homescreen.py
+++ homescreen.py
@@ -3,64 +3,84 @@ import sys
 
 pygame.init()
 
-WINDOW_WIDTH = 480
-WINDOW_HEIGHT = 853
-WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+window_width = 480
+window_height = 853
+window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Doodle Jump")
 
-FONT = pygame.font.Font(None, 50)
+font = pygame.font.Font(None, 50)
 
-# backgroundcolor
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
+# background color
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
 
-#player
-PLAYER_WIDTH = 20
-PLAYER_HEIGHT = 20
-PLAYER_X = WINDOW_WIDTH // 2 - PLAYER_WIDTH // 2
-PLAYER_Y = WINDOW_HEIGHT - PLAYER_HEIGHT
-PLAYER_RECT = pygame.Rect(PLAYER_X, PLAYER_Y, PLAYER_WIDTH, PLAYER_HEIGHT)
-VELOCITY = 0
-GRAVITY = 0.8
-IS_JUMPING = False
+# player
+player_width = 20
+player_height = 20
+player_x = window_width // 2 - player_width // 2
+player_y = window_height - player_height
+player_rect = pygame.Rect(player_x, player_y, player_width, player_height)
+velocity = 0
+gravity = 0.8
+is_jumping = False
 
-#platform
-PLATFORM_WIDTH = 100
-PLATFORM_HEIGHT = 20
-PLATFORM_X = WINDOW_WIDTH // 2 - PLATFORM_WIDTH // 2
-PLATFORM_Y = WINDOW_HEIGHT // 2
-PLATFORM_RECT = pygame.Rect(PLATFORM_X, PLATFORM_Y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
+# platform
+platform_width = 100
+platform_height = 20
+platform_x = window_width // 2 - platform_width // 2
+platform_y = window_height // 2
+platform_rect = pygame.Rect(platform_x, platform_y, platform_width, platform_height)
 
-
-#home screen
+# home screen
 def home_screen():
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if play_button_rect.collidepoint(event.pos):
                     return
 
-        WINDOW.fill(WHITE)
-        play_button = FONT.render("PLAY", True, BLACK)
-        play_button_rect = play_button.get_rect(center=WINDOW.get_rect().center)
-        WINDOW.blit(play_button, play_button_rect)
+        window.fill(white)
+        play_button = font.render("PLAY", True, black)
+        play_button_rect = play_button.get_rect(center=window.get_rect().center)
+        window.blit(play_button, play_button_rect)
 
         pygame.display.update()
 
+# game over screen
+def game_over_screen():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if retry_button_rect.collidepoint(event.pos):
+                    return
 
+        window.fill(red)
+        game_over_text = font.render("Game Over!", True, white)
+        game_over_rect = game_over_text.get_rect(center=(window_width/2, window_height/2 - 50))
+        window.blit(game_over_text, game_over_rect)
 
+        retry_button = font.render("Retry?", True, black, red)
+        retry_button_rect = retry_button.get_rect(center=(window_width/2, window_height/2 + 50))
+        pygame.draw.rect(window, black, retry_button_rect, 3)
+        window.blit(retry_button, retry_button_rect)
+
+        pygame.display.update()
+
+# game loop
 def game_loop():
-    global PLAYER_RECT, VELOCITY, IS_JUMPING
-
+    global player_rect, velocity, is_jumping
     clock = pygame.time.Clock()
     is_running = True
 
     while is_running:
+        clock.tick(60)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -69,35 +89,32 @@ def game_loop():
                 sys.exit()
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP] and not IS_JUMPING:
-            VELOCITY = -15
-            IS_JUMPING = True
-        elif keys[pygame.K_UP] and IS_JUMPING:
+        if keys[pygame.K_UP] and not is_jumping:
+            velocity = -15
+            is_jumping = True
+        elif keys[pygame.K_UP] and is_jumping:
             pass
         else:
-            IS_JUMPING = False
+            is_jumping = False
 
-        VELOCITY += GRAVITY
-        PLAYER_RECT.y += VELOCITY
+        velocity += gravity
+        player_rect.y += velocity
 
-        if PLAYER_RECT.colliderect(PLATFORM_RECT) and VELOCITY >= 0:
-            PLAYER_RECT.bottom = PLATFORM_RECT.top
-            VELOCITY = 0
-            IS_JUMPING = False
+        if player_rect.colliderect(platform_rect) and velocity >= 0:
+            player_rect.bottom = platform_rect.top
+            velocity = 0
+            is_jumping = False
 
-        if PLAYER_RECT.y > WINDOW_HEIGHT:
+        if player_rect.y > window_height:
+            game_over_screen()
             is_running = False
 
-      
-        WINDOW.fill(RED if PLAYER_RECT.y < PLATFORM_Y else WHITE)
-        pygame.draw.rect(WINDOW, BLACK, PLAYER_RECT)
-        pygame.draw.rect(WINDOW, BLACK, PLATFORM_RECT)
-        pygame.display.update()
+        window.fill(red if player_rect.y < platform_y else white)
+        pygame.draw.rect(window, black, player_rect)
+        pygame.draw.rect(window, black, platform_rect)
+        pygame.display.update() 
 
-        clock.tick(60)
-
-    pygame.quit()
-
+    home_screen()
 
 home_screen()
 game_loop()
